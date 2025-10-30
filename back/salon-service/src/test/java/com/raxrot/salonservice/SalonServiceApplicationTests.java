eaa42b168137aa42478eddc1a0cb3cc4af5fd781
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-30 13:37:15 +0000

    init empty salon-service

---
+++ back/salon-service/src/test/java/com/raxrot/salonservice/SalonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.salonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
